cf3cdee33571ce5bad02892c9defd8fc942239f2
diascw <wanessadiascw@gmail.com>
2025-05-30 00:39:28 -0300

    feat: adiciona estrutura da page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { Profile } from "@/components/templates/profile";
 import { clinicData } from "@/lib/constants";
 
 const Home: NextPage = () => {
@@ -11,6 +12,18 @@ const Home: NextPage = () => {
         <title>Vale Viver - Clínica Médica | Links</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+
+      <Profile
+        name={name}
+        description={description}
+        schedule={contact.schedule}
+        address={contact.address}
+        phone={contact.phone}
+        whatsapp={contact.whatsapp}
+        facebookUrl={social.facebook}
+        instagramUrl={social.instagram}
+        email={contact.email}
+      />
     </>
   );
 };
