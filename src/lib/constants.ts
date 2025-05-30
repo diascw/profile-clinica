a8dc246c98b28970b4a1cf134a2cbb995bcdb403
diascw <wanessadiascw@gmail.com>
2025-05-30 00:31:57 -0300

    feat: add links de contato

---
+++ src/lib/constants.ts
@@ -0,0 +1,17 @@
+export const clinicData = {
+  name: "Vale Viver",
+  description:
+    "Somos referência em dedicação, acolhimento e excelência em cada atendimento. Cuidar da sua saúde é o nosso maior patrimônio.",
+
+  contact: {
+    phone: "(33) 98808-1442",
+    whatsapp: "5533988081442",
+    email: "clinicamedicavaleviver@gmail.com",
+    address: "Rua João Marciano, 346, Santa Maria do Salto, MG.",
+    schedule: "Seg - Sex: 7h às 18h | Sáb: 8h às 12h",
+  },
+  social: {
+    facebook: "https://www.facebook.com/clinicamedicavaleviver/",
+    instagram: "https://www.instagram.com/clinicamedicavaleviver/",
+  },
+} as const;
